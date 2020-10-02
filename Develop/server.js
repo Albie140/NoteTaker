fee2bb89fe384feae03ec0bfaafa0332aef55690
Alberte Laventure <alberte.laventure@gmail.com>
2020-10-02 16:34:30 -0400

    commented out a path

--- Develop/server.js
+++ Develop/server.js
@@ -89,9 +89,9 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"))
 });
 
-app.get ("/", (req, res) => {
-    res.send(path.join(__dirname, "public/index.html"))
-    });  
+// app.get ("/", (req, res) => {
+//     res.send(path.join(__dirname, "public/index.html"))
+//     });  
 
 //Listening
 app.listen(PORT, () => {
