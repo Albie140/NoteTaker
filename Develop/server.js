2d5654ad985b860633418904fab287ab6a450472
Alberte Laventure <alberte.laventure@gmail.com>
2020-10-02 16:29:21 -0400

    Edited server file

--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,7 @@ const app = express();
 const util = require("util");
 const uniqid = require("uniqid")
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;  
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -90,9 +90,8 @@ app.get("*", (req, res) => {
 });
 
 app.get ("/", (req, res) => {
-res.send("Heroku")
-});
-
+    res.send(path.join(__dirname, "public/index.html"))
+    });  
 
 //Listening
 app.listen(PORT, () => {
